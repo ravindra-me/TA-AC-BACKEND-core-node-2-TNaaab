1bf1e9c806be1aac9feb1e14b33b481860e76e65
Ravindra Singh <ravindrarajpoot9628172@gmail.com>
2021-03-02 07:15:30 +0530

    body data refactory

--- block-BNaabd/index.js
+++ block-BNaabd/index.js
@@ -1,20 +1,36 @@
 var http = require('http');
 let server = http.createServer(handleRequest);
 let qr = require('querystring');
+// function handleRequest(req, res) {
+//   var contentType = req.headers['content-type'];
+//   var dataList = '';
+//   req.on('data', (chunk) => {
+//     dataList += chunk;
+//   });
+//   req.on('end', () => {
+//     if (contentType === 'application/json' && req.method === 'POST') {
+//       var parseData = JSON.parse(dataList);
+//       res.end(dataList);
+//     } else if (
+//       contentType === 'application/x-www-form-urlencoded' &&
+//       req.method === 'POST'
+//     ) {
+//       let parseData = qr.parse(dataList);
+//       res.end(JSON.stringify(parseData));
+//     }
+//   });
+// }
+
 function handleRequest(req, res) {
-  var contentType = req.headers['content-type'];
   var dataList = '';
   req.on('data', (chunk) => {
     dataList += chunk;
   });
   req.on('end', () => {
-    if (contentType === 'application/json' && req.method === 'POST') {
-      var parseData = JSON.parse(dataList);
+    if (req.url === '/json' && req.method === 'POST') {
+      res.setHeader('Content-Type', 'application/json');
       res.end(dataList);
-    } else if (
-      contentType === 'application/x-www-form-urlencoded' &&
-      req.method === 'POST'
-    ) {
+    } else if (req.url === '/form' && req.method === 'POST') {
       let parseData = qr.parse(dataList);
       res.end(JSON.stringify(parseData));
     }
